c217a60e7242739ea7a5da76fc5f4dace426a950
michaelanand01 <65404118+michaelanand01@users.noreply.github.com>
2020-10-08 18:53:07 -0500

    Add files via upload

--- Bolton.py
+++ Bolton.py
@@ -4,26 +4,27 @@ c_p_dry = 1005.7
 c_v_dry = 719
 eps = 0.622
 k_dry = 0.2854
+import numpy as np
 
 def sat_vapor_pressure(T):
     """Calcuates the saturation vapor pressure"""
-    e_s(T) =  6.112*np.exp(17.67T/(T+243.5))
-    return e_s(T)
+    e_s =  6.112*np.exp(17.67*T/(T+243.5))
+    return e_s
 
 def sat_vapor_temperature(e_s):
-   """Calculates the saturation vapor temperature"""
-    T = (243.5*np.log(e_s) - 440.8)/(19.48 - np.log(e_s)
+    """Calculates the saturation vapor temperature"""
+    T = (243.5*np.log(e_s) - 440.8)/(19.48 - np.log(e_s))
     return T
 
 def sat_mixing_ratio (p,T):
     """Calculates the saturation mixing ratio"""
     e_s = sat_vapor_pressure(T)
-    w_s = eps(e_s/(p-e_s))
+    w_s = eps*(e_s/(p-e_s))
     return w_s
 
 def mixing_ratio_line(p, w_s):
     """ determines the position of the mixing ratio line"""
-    e_s = (w*p)/(w+ eps)
+    e_s = (w_s*p)/(w_s+ eps)
     T = sat_vapor_temperature(e_s)
     return T
 
@@ -33,22 +34,30 @@ def RH(T, p, w):
     RH = (w/w_s)*100
     return RH
 
-def T_LCL(T,RH):
+def T_LCL(T,rh):
     """Finds the temperature of the LCL in Kelvin"""
-    Denom_1 = 1/((T)-55)
-    Denom_2 = (ln(RH/100))/2840
+    T_K = T + C_to_K
+    Denom_1 = 1/((T_K)-55)
+    Denom_2 = np.log(rh/100)/2840
     T_LCL =(1/(Denom_1 - Denom_2)) + 55
     return T_LCL
 
 def theta_dry(theta, p, p_0=1000.0):
     """Calculates the dry adiabats"""
-    theta_dry = T(1000/p)**(k_dry)
+    theta_dry = theta*(p/p_0)**(k_dry)
     return theta_dry
 
 def pseudoeq_potential_T(T, p, w, p_0=1000.0):
     """Calcuates the pseudoadiabatic equivalent potential temperature by taking account the
     moisture content in the air"""
-    pseudo_adia_equiv_pot_T = (T(1000/p)**k_dry)*(np.exp(((3.376/T)-0.00254)*w(1+(0.81*10**-3)*w))
+    T_K = T + C_to_K
+    rh = RH(T,p,w)
+    Temp_LCL = T_LCL(T,rh)
+    term1 = (T_K*(1000/p)**k_dry)
+    term2 = (3.376/Temp_LCL)-0.00254
+    term3 = w*(1+(0.81*10**-3)*w)
+    term4 = np.exp(term2*term3)
+    pseudo_adia_equiv_pot_T = term1 * term4
     return pseudo_adia_equiv_pot_T
 
 def theta_ep_field (T, p, p_0=1000.0):
--- skewt.py
+++ skewt.py
@@ -27,7 +27,7 @@ def T_from_xp (x,p):
     T = x + skew_slope*np.log(p)
     return T
 
-def p _from_y (y):
+def p_from_y (y):
     """Converting the vertical coordinate height value to a pressure"""
     p = np.exp(-y)
     return p
@@ -65,16 +65,18 @@ c_p_dry = 1004  #specific heat capacity at constant pressure
 p_levels =np.arange(1000, 150-50, -50)
 T_C_levels = np.arange(-80,40,10)
 T_levels = T_C_levels + C_to_K
-theta_levels = np.arange(-40,100,10)
-theta.ep_levels = theta_levels.copy()
+theta_C_levels = np.arange(-40,100,10)
+theta_levels = theta_C_levels + C_to_K
+theta_ep_levels = theta_levels.copy()
 mixing_ratios = 0.001* np.asarray([0.4,1,2,3,5,8,12,16,20])
 
 p_all = np.arange(p_bottom, p_top, -1)
 y_p_levels  = y_from_p (p_levels)
 y_all_p = y_from_p (p_all)
-x_T_levels = [x_from_TP(Ti, p_all) for Ti in T_levels]
-x_thetas = [x_fromTP(Bolton.theta_dry(theta_i, p_all), p_all) for theta_i in theta_levels]
-x_mixing_ratios = [x_fromTP(Bolton.mixing_ratio_line(mixing_ratios_i, p_all), p_all) for mixing_ratios_i in mixing_ratios]
+x_T_levels = [x_from_Tp(Ti, p_all) for Ti in T_levels]
+
+x_thetas = [x_from_Tp(Bolton.theta_dry(theta_i, p_all), p_all) for theta_i in theta_levels]
+x_mixing_ratios = [x_from_Tp(Bolton.mixing_ratio_line(mixing_ratios_i, p_all)+C_to_K, p_all) for mixing_ratios_i in mixing_ratios]
 
 mesh_T, mesh_p = np.meshgrid(np.arange (-60.0, T_levels.max()-C_to_K+0.1, 0.1), p_all)
 theta_ep_mesh = Bolton.theta_ep_field(mesh_T, mesh_p)
@@ -82,14 +84,17 @@ theta_ep_mesh = Bolton.theta_ep_field(mesh_T, mesh_p)
 
 def theta_e(T,p):
     """Calculate equivalent potential temperature by allowing latent heat to vary with temp"""
+    k_dry = 0.2854
     T = T + C_to_K
-    Lv = 2.501 - 0.00237T)*10**6
-    es = bolton.sat_vapor_pressure(T)
+    Lv = (2.501 - 0.00237*T)*10**6
+    es = Bolton.sat_vapor_pressure(T)
     pv = es
     pd = p -pv
-    w = bolton.sat_mixing_ratio(p,T)
+    w = Bolton.sat_mixing_ratio(p,T)
     Theta_E = (T*(1000/pd)**(k_dry)) * np.exp((Lv*w)/(4190*T))
     return Theta_E
+
+#theta_e_mesh = theta_e(mesh_T, mesh_p)
 
 
 fig = plt.figure(figsize=(12,12))
@@ -105,25 +110,23 @@ for x_T in x_T_levels:
     ax.plot(x_T, y_all_p, color=(1.0, 0.5, 0.5))
 
 for x_theta in x_thetas:
-    ax.plot(x_theta, y_all_p, color-(1.0, 0.7, 0.7))
+    ax.plot(x_theta, y_all_p, color=(1.0, 0.7, 0.7))
 
 for x_mixing_ratio in x_mixing_ratios:
-    good = all_P >= 600   # restrict mixing ratio lines to below 600 mb
+    good = p_all >= 600   # restrict mixing ratio lines to below 600 mb
     ax.plot (x_mixing_ratio [good], y_all_p[good], color=(0.8, 0.8, 0.6))
 
 n_moist = len(theta_ep_levels)
 moist_colors = ((0.6, 0.9, 0.7),)*n_moist
-ax.contour(x_from_TP(mesh_T+C_to_K, mesh_p), y-from_p(mesh_p), theta_ep_mesh, theta_ep_levels, colors = moist_colors)
-
-# Your code for plotting theta_ (reversible)
-
+ax.contour(x_from_Tp(mesh_T+C_to_K, mesh_p), y_from_p(mesh_p), theta_ep_mesh, theta_ep_levels, colors = moist_colors)
+#ax.contour(x_from_Tp(mesh_T+C_to_K, mesh_p), y_from_p(mesh_p), theta_e_mesh, theta_e, colors = moist_colors)
 ax.axis((x_min, x_max, y_min, y_max))
 
 plt.show()
 
 skew_grid_helper = GridHelperCurveLinear((from_thermo, to_thermo))
 
-subplot(grid_helper)
+ax = Subplot(fig,1,1,1,grid_helper = skew_grid_helper)
 
 def format_coord(x,y):
     T, p = to_thermo(x,y)
