6f17b862b0a5169c37293bc1ab53d73b9b2ead8f
michaelanand01 <mianand@ttu.edu>
2020-10-07 19:29:14 -0500

    skewT project calculations for #4

--- Bolton.py
+++ Bolton.py
@@ -17,12 +17,14 @@ def sat_vapor_temperature(e_s):
 
 def sat_mixing_ratio (p,T):
     """Calculates the saturation mixing ratio"""
-    e_s = 6.112*np.exp(17.67T/(T+243.5))
+    e_s = sat_vapor_pressure(T)
     w_s = eps(e_s/(p-e_s))
     return w_s
 
 def mixing_ratio_line(p, w_s):
     """ determines the position of the mixing ratio line"""
+    e_s = (w*p)/(w+ eps)
+    T = sat_vapor_temperature(e_s)
     return T
 
 def RH(T, p, w):
@@ -37,3 +39,21 @@ def T_LCL(T,RH):
     Denom_2 = (ln(RH/100))/2840
     T_LCL = 1/(Denom_1 - Denom_2)
     return T_LCL
+
+def theta_dry(theta, p, p_0=1000.0):
+    """Calculates the dry adiabats"""
+    theta_dry = T(1000/p)**(k_dry)
+    return theta_dry
+
+def pseudoeq_potential_T(T, p, w, p_0=1000.0):
+    """Calcuates the pseudoadiabatic equivalent potential temperature by taking account the
+    moisture content in the air"""
+    pseudo_adia_equiv_pot_T = (T(1000/p)**k_dry)*(np.exp(((3.376/T)-0.00254)*w(1+(0.81*10**-3)*w))
+    return pseudo_adia_equiv_pot_T
+
+def theta_ep_field (T, p, p_0=1000.0):
+    """Determine and calculate the moist adiabat field"""
+    w_s = sat_mixing_ratio (p,T)
+    theta_ep = pseudoeq_potential_T(T, p, w_s, p_0=1000.0)
+    return theta_ep
+
