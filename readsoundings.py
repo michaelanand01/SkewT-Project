457b344ec5a4fabe4debedbf2830527f512525a9
michaelanand01 <65404118+michaelanand01@users.noreply.github.com>
2020-10-08 19:00:06 -0500

    Add files via upload

---
+++ readsoundings.py
@@ -0,0 +1,10 @@
+def parse_SPC(filename, skip_rows=6):
+    dtype = [ ('p', float),              #pressure, mb
+              ('z', float),              #altitude, m
+              ('T', float),              #temperature, C
+              ('Td", float),             #dewpoint, C
+              ('wind_dir', float),       #wind direction, egrees
+              ('wind_spd', float)        #wind speed, knots
+              ]
+    data = np.genfromtxt(filename, dtype=dtype, skip_header=skip_rows, delimiter=',')
+    return data
